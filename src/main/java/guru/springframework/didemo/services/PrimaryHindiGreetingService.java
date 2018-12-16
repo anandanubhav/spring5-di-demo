18856feb20f1ebcedbfb14d208c72b59e66e1484
Anubhav Anand <aa7784@gmail.com>
2018-12-16 23:57:41 +0530

    spring profiles

--- src/main/java/guru/springframework/didemo/services/PrimaryHindiGreetingService.java
+++ src/main/java/guru/springframework/didemo/services/PrimaryHindiGreetingService.java
@@ -5,11 +5,11 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
+@Profile("hn")
 @Primary
-@Profile("en")
-public class PrimaryGreetingService implements GreetingService {
+public class PrimaryHindiGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
-        return "Hello - Primary Greeting Service";
+        return "Namaste - I am Hindi Greeting";
     }
 }
